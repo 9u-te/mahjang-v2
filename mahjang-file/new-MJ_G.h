35dd5f8d745b4544641eea2da2bafdb77b5c9bf9
9u-te <sup894ma@gmail.com>
2026-04-06 14:25:25 +0900

    new-MJ_G.h を更新

---
+++ mahjang-file/new-MJ_G.h
@@ -0,0 +1,14 @@
+#include "new-MJ_P"
+
+class Game{
+    public:
+        std::vector<MAHJANG_HI> YAMA;
+        std::vector<MAHJANG_HI> WANGPAI;
+        
+        std::vector<MAHJANG_HI> KAWA0, KAWA1, KAWA2, KAWA3;
+        
+        PLAYER player0, player1, player2, player3;
+        
+        
+        
+};
